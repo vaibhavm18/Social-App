89a639b9b346a6a654b97389212ea60ca06fdc41
vaibhav <vaibhavnayak018@gmail.com>
2023-12-21 20:09:33 +0530

    Initializing db and env

---
+++ common/db.go
@@ -0,0 +1,38 @@
+package common
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var db *mongo.Database
+
+func GetDBCollection(col string) *mongo.Collection {
+	return db.Collection(col)
+}
+
+func InitDB() error {
+	uri := os.Getenv("MONGO_URL")
+
+	if uri == "" {
+		return errors.New("You must set your 'MONGO_URL' env variable.")
+	}
+
+	client, err := mongo.Connect(context.Background(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		return err
+	}
+
+	db = client.Database("blind")
+
+	return nil
+}
+
+func CloseDB() error {
+	return db.Client().Disconnect(context.Background())
+}
---
+++ common/env.go
@@ -0,0 +1,15 @@
+package common
+
+import (
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error {
+	err := godotenv.Load()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
