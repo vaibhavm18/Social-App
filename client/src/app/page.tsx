6aba4b7d3972c04e701b1fe4bae118ab9de5b790
vaibhav <vaibhavnayak018@gmail.com>
2024-02-20 01:14:56 +0530

    Reintialized client with next js

---
+++ client/src/app/page.tsx
@@ -0,0 +1,27 @@
+export default function Home() {
+  return (
+    <main className="max-w-7xl mx-auto border h-screen p-4 relative">
+      <section className="border h-full p-2 relative flex flex-col  ">
+        <div className="p-5 border">Hello God!</div>
+        <div className=" overflow-auto flex-auto relative">
+          <div
+            className="absolute overflow-auto border border-yellow-500 w-full
+          h-full flex flex-col"
+          >
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+            <div className="p-8">1</div>
+          </div>
+        </div>
+        <div className="p-5 border">Hello God!</div>
+      </section>
+    </main>
+  );
+}
