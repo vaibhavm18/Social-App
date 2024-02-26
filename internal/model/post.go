8add25d31a2970f804631c70ee2687050c88b6cb
vaibhav <vaibhavnayak018@gmail.com>
2024-02-27 02:09:49 +0530

    creating post rotes

--- internal/model/post.go
+++ internal/model/post.go
@@ -1,11 +1,17 @@
 package model
 
 import (
+	"context"
+	"errors"
+	"fmt"
 	"time"
 
 	// "github.com/vaibhavm18/go-blind/internal/config"
 	"github.com/faceair/jio"
+	"github.com/vaibhavm18/go-blind/internal/config"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type PostModel struct {
@@ -13,21 +19,49 @@ type PostModel struct {
 	AuthorID    primitive.ObjectID   `json:"authorId" bson:"authorId"`
 	Title       string               `json:"title" bson:"title"`
 	Description string               `json:"description" bson:"description"`
-	Likes       []primitive.ObjectID `json:"likes" bson:"likes"`
-	Dislikes    []primitive.ObjectID `json:"dislikes" bson:"dislikes"`
-	CreatedAt   time.Time            `json:"createdAt" bson:"createdAt"`
+	Likes       []primitive.ObjectID `json:"likes,omitempty" bson:"likes,omitempty"`
+	Dislikes    []primitive.ObjectID `json:"dislikes,omitempty" bson:"dislikes,omitempty"`
+	CreatedAt   time.Time            `json:"createdAt,omitempty" bson:"createdAt,omitempty"`
 }
 
-type CommentModel struct {
-	ID        primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
-	PostID    primitive.ObjectID   `json:"postId" bson:"postId"`
-	CreatedAt time.Time            `json:"createdAt" bson:"createdAt"`
-	Comment   string               `json:"comment" bson:"comment"`
-	Likes     []primitive.ObjectID `json:"likes" bson:"likes"`
+type PostInput struct {
+	UserID primitive.ObjectID `json:"userId" bson:"userId"`
+	PostID primitive.ObjectID `json:"postId" bson:"postId"`
 }
 
-func CreatePost(post PostModel) {
-	// col := config.GetDBCollection("Posts")
+// type CommentModel struct {
+// 	ID        primitive.ObjectID   `json:"id,omitempty" bson:"_id,omitempty"`
+// 	PostID    primitive.ObjectID   `json:"postId" bson:"postId"`
+// 	CreatedAt time.Time            `json:"createdAt" bson:"createdAt"`
+// 	Comment   string               `json:"comment" bson:"comment"`
+// 	Likes     []primitive.ObjectID `json:"likes" bson:"likes"`
+// }
+
+func CreatePost(post PostModel) (PostModel, error) {
+	col := config.GetDBCollection("Posts")
+
+	err := validatePostInput(post)
+
+	if err != nil {
+		return PostModel{}, err
+	}
+
+	post.CreatedAt = time.Now()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	res, err := col.InsertOne(ctx, post)
+
+	if err != nil {
+		fmt.Println(err)
+		return PostModel{}, err
+	}
+
+	fmt.Println("create post", res)
+	return PostModel{}, nil
+
 }
 
 func validatePostInput(post PostModel) error {
@@ -38,56 +72,196 @@ func validatePostInput(post PostModel) error {
 	}
 
 	_, err = jio.ValidateJSON(&data, jio.Object().Keys(jio.K{
-		"username": jio.String().Required().Min(3).Max(10),
-		// "password": jio.String().Min(6),
+		"title":       jio.String().Required().Min(12).Max(60),
+		"description": jio.String().Min(40).Max(1000),
 	}))
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
-func DeletePost() {
+func GetPosts(page int) ([]PostModel, error) {
+	collection := config.GetDBCollection("Posts")
+	perPage := 15
+	skip := (page - 1) * perPage
 
+	opts := options.Find()
+
+	opts.SetSkip(int64(skip))
+	opts.SetLimit(int64(perPage))
+	opts.SetSort(bson.D{{Key: "createdAt", Value: -1}})
+
+	res, err := collection.Find(context.Background(), bson.M{}, opts)
+
+	if err != nil {
+		return nil, err
+	}
+
+	var posts []PostModel
+
+	for res.Next(context.Background()) {
+		var post PostModel
+		if err := res.Decode(&post); err != nil {
+			return nil, err
+		}
+
+		posts = append(posts, post)
+	}
+
+	if err := res.Err(); err != nil {
+		return nil, err
+	}
+
+	res.Close(context.Background())
+
+	fmt.Println("posts", posts)
+
+	return posts, nil
 }
 
-func GetAllpost() {
+func LikePost(input PostInput) error {
+	col := config.GetDBCollection("Posts")
 
+	filter := bson.M{"_id": input.PostID}
+
+	var post PostModel
+
+	err := col.FindOne(context.Background(), filter).Decode(&post)
+
+	if err != nil {
+		return err
+	}
+
+	for _, likedBy := range post.Likes {
+		if likedBy == input.UserID {
+			return errors.New("User already liked the post")
+		}
+	}
+	update := bson.M{"$addToSet": bson.M{"likes": input.UserID}}
+	_, err = col.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	err = RemoveDislike(input)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func GetPostById() {
+func UnlikePost(input PostInput) error {
+	col := config.GetDBCollection("Posts")
 
+	filter := bson.M{"_id": input.PostID}
+
+	var post PostModel
+
+	err := col.FindOne(context.Background(), filter).Decode(&post)
+
+	if err != nil {
+		return err
+	}
+
+	isLiked := false
+	for _, likedBy := range post.Likes {
+		if likedBy == input.UserID {
+			isLiked = true
+		}
+	}
+
+	if isLiked {
+		return errors.New("User did not liked this post")
+	}
+
+	update := bson.M{"$pull": bson.M{"likes": input.UserID}}
+	_, err = col.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func GetMyPost() {
+func DislikePost(input PostInput) error {
 
+	col := config.GetDBCollection("Posts")
+
+	filter := bson.M{"_id": input.PostID}
+
+	var post PostModel
+
+	err := col.FindOne(context.Background(), filter).Decode(&post)
+
+	if err != nil {
+		return err
+	}
+
+	for _, dislikedBy := range post.Dislikes {
+		if dislikedBy == input.UserID {
+			return errors.New("User already diliked the post")
+		}
+	}
+	update := bson.M{"$addToSet": bson.M{"dislikes": input.UserID}}
+	_, err = col.UpdateOne(context.Background(), filter, update)
+
+	if err != nil {
+		return err
+	}
+
+	err = UnlikePost(input)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
-func LikePost() {
+func RemoveDislike(input PostInput) error {
 
-}
+	col := config.GetDBCollection("Posts")
 
-func UnlikePost() {
+	filter := bson.M{"_id": input.PostID}
 
-}
+	var post PostModel
 
-func DislikePost() {
+	err := col.FindOne(context.Background(), filter).Decode(&post)
 
-}
+	if err != nil {
+		return err
+	}
 
-func UnDislikePost() {
+	isAlreadyDislike := false
+	for _, dislikedBy := range post.Dislikes {
+		if dislikedBy == input.UserID {
+			isAlreadyDislike = true
+		}
+	}
 
-}
+	if isAlreadyDislike {
+		return errors.New("User already diliked the post")
+	}
 
-func CreateComment() {
+	update := bson.M{"$pull": bson.M{"dislikes": input.UserID}}
+	_, err = col.UpdateOne(context.Background(), filter, update)
 
-}
+	if err != nil {
+		return err
+	}
 
-func DeleteComment() {
+	err = UnlikePost(input)
 
-}
+	if err != nil {
+		return err
+	}
 
-func LikeComment() {
-
-}
-
-func UnlikeComment() {
-
+	return nil
 }
--- internal/router/post.go
+++ internal/router/post.go
@@ -8,9 +8,59 @@ import (
 func AddPostGroup(app *fiber.App) {
 	postGroup := app.Group("/api/v1/post")
 	postGroup.Use(util.VerifyUser)
-	postGroup.Get("/", postCheck)
+
+	postGroup.Get("/", getPosts)
+	postGroup.Post("/create", createPost)
+	postGroup.Put("/like", likePost)
+	postGroup.Put("/dislike", dislikePost)
+	postGroup.Delete("/removeLike", removelike)
+	postGroup.Delete("/removedislike", removedislike)
 }
 
 func postCheck(c *fiber.Ctx) error {
 	return c.Status(200).JSON(fiber.Map{"message": "Hello, World 👋!"})
+}
+
+func createPost(c *fiber.Ctx) error {
+	// TODO: create Post
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func getPosts(c *fiber.Ctx) error {
+
+	// TODO: get 10 Post at a one request using pagination
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func likePost(c *fiber.Ctx) error {
+
+	// TODO: Like the post
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func dislikePost(c *fiber.Ctx) error {
+	// TODO: dislike the post
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func removelike(c *fiber.Ctx) error {
+	// TODO: removelike the post
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
+}
+
+func removedislike(c *fiber.Ctx) error {
+	// TODO: removedislike the post
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Success",
+	})
 }
