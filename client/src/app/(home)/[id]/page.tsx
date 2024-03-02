9b0e1fe0516f86ba18b4490e8eaf56d2738ced94
vaibhav <vaibhavnayak018@gmail.com>
2024-03-03 00:12:55 +0530

    new react client

--- client/src/app/(home)/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Post from "@/components/Post";
-import { useParams } from "next/navigation";
-
-export default function Posts() {
-  const { id } = useParams();
-
-  return <Post />;
-}
--- client/src/app/(home)/post/page.tsx
+++
@@ -1,5 +0,0 @@
-import PostForm from "@/components/PostForm";
-
-export default function page() {
-  return <PostForm />;
-}
