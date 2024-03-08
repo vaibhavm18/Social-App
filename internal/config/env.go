6b7b2629a8ca75a7f36f03466ee5d1b3ca3babd6
vaibhav <vaibhavnayak018@gmail.com>
2024-03-08 19:06:58 +0530

    fixing env bug

--- internal/config/env.go
+++ internal/config/env.go
@@ -1,13 +1,22 @@
 package config
 
 import (
+	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() error {
-	err := godotenv.Load(filepath.Join(".env"))
+	path, err := os.Getwd()
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println(path)
+	err = godotenv.Load(filepath.Join(path, ".env"))
 
 	if err != nil {
 		return err
